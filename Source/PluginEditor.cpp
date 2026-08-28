65697f3649ab4636ddb4523f42327fe75a8a8997
Yisus1Chrust <155608720+Yisus1chrust@users.noreply.github.com>
2026-08-28 10:51:54 -0600

    Update PluginEditor.cpp

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -7,7 +7,7 @@ namespace photosynth
     {
         addAndMakeVisible (webView);
         setSize (1280, 760);
-        webView.goToURL ("http://localhost:5173");
+        webView.goToURL ("http://localhost:3000");
     }
 
     PhotoSynthAudioProcessorEditor::~PhotoSynthAudioProcessorEditor() = default;
