50f40b0637b6f94d40b1b667451c9c00b2c04033
Yisus1Chrust <155608720+Yisus1chrust@users.noreply.github.com>
2026-08-29 17:44:29 -0600

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -3,37 +3,15 @@
 
 namespace photosynth
 {
-    PhotoSynthAudioProcessorEditor::PhotoSynthAudioProcessorEditor (PhotoSynthAudioProcessor& p)
-        : AudioProcessorEditor (&p), audioProcessor (p)
+    // ... your audio processing code (prepareToPlay, processBlock, etc.) ...
+
+    bool PhotoSynthAudioProcessor::hasEditor() const
     {
-        macro1Slider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
-        macro1Slider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 20);
-        addAndMakeVisible (macro1Slider);
-
-        macro2Slider.setSliderStyle (juce::Slider::RotaryHorizontalVerticalDrag);
-        macro2Slider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 20);
-        addAndMakeVisible (macro2Slider);
-
-        setSize (600, 400); 
+        return true; // This tells JUCE your plugin has a graphical interface
     }
 
-    PhotoSynthAudioProcessorEditor::~PhotoSynthAudioProcessorEditor() = default;
-
-    void PhotoSynthAudioProcessorEditor::paint (juce::Graphics& g)
+    juce::AudioProcessorEditor* PhotoSynthAudioProcessor::createEditor()
     {
-        g.fillAll (juce::Colour (0xff111318)); 
-
-        g.setColour (juce::Colours::white);
-        g.setFont (24.0f);
-        g.drawFittedText ("Photo Synth", getLocalBounds().removeFromTop (50), juce::Justification::centred, 1);
-    }
-
-    void PhotoSynthAudioProcessorEditor::resized()
-    {
-        auto area = getLocalBounds().reduced (40);
-        area.removeFromTop (40); 
-
-        macro1Slider.setBounds (area.removeFromLeft (area.getWidth() / 2));
-        macro2Slider.setBounds (area);
+        return new PhotoSynthAudioProcessorEditor (*this);
     }
 }
