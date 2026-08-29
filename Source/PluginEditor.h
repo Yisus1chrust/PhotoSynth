d4211b36b344f55c21bbbb01ef38997d4bf3ded1
Yisus1Chrust <155608720+Yisus1chrust@users.noreply.github.com>
2026-08-29 17:10:27 -0600

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -18,13 +18,8 @@ namespace photosynth
     private:
         PhotoSynthAudioProcessor& audioProcessor;
 
-        // Standard Native JUCE UI Controls
         juce::Slider macro1Slider;
         juce::Slider macro2Slider;
-
-        // Uncomment these later to automatically link your UI to your audio parameters
-        // std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> macro1Attachment;
-        // std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> macro2Attachment;
 
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PhotoSynthAudioProcessorEditor)
     };
